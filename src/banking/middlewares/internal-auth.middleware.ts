12a4a7dd0e8c8f163a40556f01dbb3ae873791e6
victoriabonanni <victoria.ayui@gmail.com>
2025-08-19 20:46:19 -0300

    add: internal auth middleware impl

---
+++ src/banking/middlewares/internal-auth.middleware.ts
@@ -0,0 +1,26 @@
+/** npm imports */
+import { Injectable, InternalServerErrorException, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NextFunction, Request, Response } from 'express';
+
+/* local imports */
+import { ConfigKey } from '../../config/enum';
+
+@Injectable()
+export class InternalAuthMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfigService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const expectedSecret = this.configService.get<string>(ConfigKey.INTERNAL_AUTH_SECRET_BANKING_SERVICE);
+    if (!expectedSecret) {
+      throw new InternalServerErrorException('Missing INTERNAL_AUTH_SECRET_BANKING_SERVICE in Banking Service configuration');
+    }
+
+    const securedHeader = req.headers['x-internal-auth'];
+    if (securedHeader !== expectedSecret) {
+      throw new UnauthorizedException('Unauthorized internal request. This service should only be accessed by the API Gateway.');
+    }
+
+    next();
+  }
+}
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,5 +5,6 @@ export default () => ({
     [ConfigKey.PORT]: parseInt(process.env.PORT ?? '4000', 10),
     [ConfigKey.NODE_ENV]: process.env.NODE_ENV ?? 'development',
     [ConfigKey.SDK_FINANCE_BASE_URL]: process.env.SDK_FINANCE_BASE_URL,
+    [ConfigKey.INTERNAL_AUTH_SECRET_BANKING_SERVICE]: process.env.INTERNAL_AUTH_SECRET_BANKING_SERVICE,
   });
-  
+    
